da18a4b3dc5f72506835c81ca98b469ea5b9490e
d1zm4as <pedrofamilia364@gmail.com>
2023-08-09 10:11:18 -0300

    +1

--- better_ip_sniffer/src/main.rs
+++ better_ip_sniffer/src/main.rs
@@ -5,19 +5,19 @@ use std::sync::mpsc::{channel, Sender};
 use tokio::net::TcpStream;
 use tokio::task;
 
-// Max IP Port.
+// porta maxima
 const MAX: u16 = 65535;
 
-// Address fallback.
+// endereço de fallback.
 const IPFALLBACK: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
 
-// CLI Arguments.
+// argumentos da cli.
 #[derive(Debug, Clone, Bpaf)]
 #[bpaf(options)]
 pub struct Arguments {
-    // Address argument.  Accepts -a and --address and an IpAddr type. Falls back to the above constant.
+    // aceita -a ou --adress como argumento
     #[bpaf(long, short, argument("Address"), fallback(IPFALLBACK))]
-    /// The address that you want to sniff.  Must be a valid ipv4 address.  Falls back to 127.0.0.1
+    /// o endereço para o processo de sniff, tem que ser um ipv4 valido
     pub address: IpAddr,
     #[bpaf(
         long("start"),
@@ -25,7 +25,7 @@ pub struct Arguments {
         guard(start_port_guard, "Must be greater than 0"),
         fallback(1u16)
     )]
-    /// The start port for the sniffer. (must be greater than 0)
+    /// a porta inicial do sniff
     pub start_port: u16,
     #[bpaf(
         long("end"),
@@ -33,7 +33,7 @@ pub struct Arguments {
         guard(end_port_guard, "Must be less than or equal to 65535"),
         fallback(MAX)
     )]
-    /// The end port for the sniffer. (must be less than or equal to 65535)
+    /// o valor final da porta, tem quer menor ou igual a const max
     pub end_port: u16,
 }
 
@@ -46,39 +46,38 @@ fn end_port_guard(input: &u16) -> bool {
 }
 
 
-// Scan the port.
+// Scaneia a porta
 async fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr) {
-    // Attempts Connects to the address and the given port.
+    // tenta a conexaõ com o endereço e porta
     match TcpStream::connect(format!("{}:{}", addr, start_port)).await {
-        // If the connection is successful, print out a . and then pass the port through the channel.
+        // se for sucesso (caso a porta esteja aberta), vai printar um .
         Ok(_) => {
             print!(".");
             io::stdout().flush().unwrap();
             tx.send(start_port).unwrap();
         }
-        // If the connection is unsuccessful, do nothing. Means port is not open.
+        // caso contrario a porta esta fechada
         Err(_) => {}
     }
 }
 
 #[tokio::main]
 async fn main() {
-    // collect the arguments.
+    // lendo os argumentos
     let opts = arguments().run();
-    // Initialize the channel.
+    // iniciando o canal
     let (tx, rx) = channel();
-    // Iterate through all of the ports (based on user input) so that we can spawn a single task for each.
-    // (Much faster than before because it uses green threads instead of OS threads.)
+    // interando sobre todas as portas e criando uma task para cada.
     for i in opts.start_port..opts.end_port {
         let tx = tx.clone();
 
         task::spawn(async move { scan(tx, i, opts.address).await });
     }
-    // Create the vector for all of the outputs.
+    // vetor de outputs
     let mut out = vec![];
-    // Drop the tx clones.
+    
     drop(tx);
-    // Wait for all of the outputs to finish and push them into the vector.
+    // aguarda todos os scans acabarem e coloca no vetor out.
 
     for p in rx {
         out.push(p);
@@ -87,7 +86,7 @@ async fn main() {
     println!("");
     out.sort();
     for v in out {
-        // Iterate through the outputs and print them out as being open.
+        // printando os outputs(portas abertas)
         println!("{} is open", v);
     }
 }
--- better_ip_sniffer/target/debug/deps/betterIpSniffer-14b5f14b2219cf3b.d
+++ better_ip_sniffer/target/debug/deps/betterIpSniffer-14b5f14b2219cf3b.d
@@ -1,5 +1,5 @@
-/home/me/Documents/RustProjects/betterIpSniffer/target/debug/deps/betterIpSniffer-14b5f14b2219cf3b: src/main.rs
+/home/me/Documents/RustProjects/better_ip_sniffer/target/debug/deps/betterIpSniffer-14b5f14b2219cf3b: src/main.rs
 
-/home/me/Documents/RustProjects/betterIpSniffer/target/debug/deps/betterIpSniffer-14b5f14b2219cf3b.d: src/main.rs
+/home/me/Documents/RustProjects/better_ip_sniffer/target/debug/deps/betterIpSniffer-14b5f14b2219cf3b.d: src/main.rs
 
 src/main.rs:
