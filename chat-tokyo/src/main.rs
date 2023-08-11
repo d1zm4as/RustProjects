15f7f8dea9209bdd9ccad5af2bee698341296e0b
d1zm4as <pedrofamilia364@gmail.com>
2023-08-11 18:31:24 -0300

    ups

--- chat-tokyo/src/main.rs
+++ chat-tokyo/src/main.rs
@@ -1,6 +1,7 @@
-
-use tokio::net::TcpListener;
-
+use tokio::{
+    io::{AsyncWriteExt,BufReader,AsyncBufReadExt},
+    net::TcpListener, 
+};
 
 
 
@@ -12,9 +13,32 @@ async fn main() {
 
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (socket, _addr) =  listener.accept().await.unwrap();
+    let (mut socket, _addr) =  listener.accept().await.unwrap();
+
+    let (reader, mut writer) = socket.split();
+
+    let mut reader = BufReader::new(reader);
+
+    let mut line = String::new();
+    loop{
+        
+        
+
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read  == 0{
+            break;
+        }
+
+        writer.write_all(&line.as_bytes()).await.unwrap();
+    
+}
+
+
+
+
 
 
 
 
 }
+
